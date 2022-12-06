a33939f2593214e78eaf7c180bbbebe675b782dd
warren <2410818122@qq.com>
2022-12-06 21:42:30 +0800

    acids孵化

--- src/main/java/pers/warren/ioc/CokeApplication.java
+++ src/main/java/pers/warren/ioc/CokeApplication.java
@@ -10,6 +10,8 @@ import pers.warren.ioc.inject.Inject;
 import pers.warren.ioc.loader.Loader;
 import pers.warren.ioc.util.ScanUtil;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.*;
 
 /**
@@ -114,7 +116,33 @@ public class CokeApplication {
      */
     private static void start() {
         startTimeMills = System.currentTimeMillis();
+        disableAccessWarnings();
         printBanner();
+    }
+
+
+
+
+    /**
+     * 忽略非法反射警告  适用于jdk11
+     */
+    @SuppressWarnings("unchecked")
+    public static void disableAccessWarnings() {
+        try {
+            Class unsafeClass = Class.forName("sun.misc.Unsafe");
+            Field field = unsafeClass.getDeclaredField("theUnsafe");
+            field.setAccessible(true);
+            Object unsafe = field.get(null);
+
+            Method putObjectVolatile = unsafeClass.getDeclaredMethod("putObjectVolatile", Object.class, long.class, Object.class);
+            Method staticFieldOffset = unsafeClass.getDeclaredMethod("staticFieldOffset", Field.class);
+
+            Class loggerClass = Class.forName("jdk.internal.module.IllegalAccessLogger");
+            Field loggerField = loggerClass.getDeclaredField("logger");
+            Long offset = (Long) staticFieldOffset.invoke(unsafe, loggerField);
+            putObjectVolatile.invoke(unsafe, loggerClass, offset, null);
+        } catch (Exception ignored) {
+        }
     }
 
     /**
